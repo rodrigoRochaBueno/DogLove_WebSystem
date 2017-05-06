9d258215c157ce4c60b94a1b72caa68035b96e3d
Rodrigo Rocha Bueno <rodrigo.rochabueno2@gmail.com>
2017-05-06 01:40:52 -0300

    salvando e listando do banco de dados

--- src/main/resources/static/JavaScript/controllers/cadastroController.js
+++ src/main/resources/static/JavaScript/controllers/cadastroController.js
@@ -1,3 +1,46 @@
-appDogLove.controller('cadastroController', function($scope, $http){
+appDogLove.controller('cadastroController', function($scope, $http) {
+
+	$scope.cliente = {};
+	$scope.listaClientes = [];
+
+	$scope.listarClientes = function() {
+		$http.get("/listarClientes").then(function(response) {
+
+			$scope.listaClientes = response.data;
+
+			console.log(response.data);
+
+		}, function(response) {
+
+			window.alert("Oops! Ocorreu um erro ao tentar listar os clientes");
+			console.log(response.data);
+
+		});
+
+	};
+
+	$scope.salvarCliente = function() {
+		$http({
+			method : "POST",
+			url : "http://localhost:8092/salvar",
+			data : $scope.cliente
+		}).then(
+				function(response) {
+
+					window.alert($scope.cliente.nome + " "
+							+ "foi adicionado a lista de clientes");
+					console.log(response.data);
+
+					$scope.listarClientes();
+
+				}, function() {
+
+					console.log(response.data);
+
+				});
+
+	};
 	
+	$scope.listarClientes();
+
 });
